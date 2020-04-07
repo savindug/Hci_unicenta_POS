c6e0c0d4658ac3e7300f1ebe357d6e3977e936d7
Ama-Thilakarathne <lil.birdie.me.15@gmail.com>
2020-04-07 20:55:54 +0530

    variable created

--- src/main/java/com/openbravo/pos/sales/DataLogicReceipts.java
+++ src/main/java/com/openbravo/pos/sales/DataLogicReceipts.java
@@ -39,7 +39,7 @@ import java.util.List;
 public class DataLogicReceipts extends BeanFactoryDataSingle {
     
     private Session s;
-    
+    private int amasha;
     /** Creates a new instance of DataLogicReceipts */
     public DataLogicReceipts() {
     }
